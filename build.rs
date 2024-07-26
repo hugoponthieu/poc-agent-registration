5ae3f5a6103a4eab4018d28f27d7fd1f5940560e
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-07-26 23:29:40 +0200

    wip

---
+++ build.rs
@@ -0,0 +1,5 @@
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = tonic_build::compile_protos("proto/registration.proto");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use proto::agent_server::{Agent, AgentServer};
+mod proto {
+    tonic::include_proto!("registration");
+}
+
+#[derive(Debug, Default)]
+struct RegistrationService {}
+#[tonic::async_trait]
+impl Agent for RegistrationService {
+    async fn register_agent(
+        &self,
+        request: tonic::Request<proto::Health>,
+    ) -> Result<tonic::Response<proto::RegisterAgentResponse>, tonic::Status> {
+    }
+}
 fn main() {
     println!("Hello, world!");
 }

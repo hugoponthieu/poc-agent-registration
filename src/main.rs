ced928ce3bd41d16574cab9a6c3929e5567d7b26
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-07-27 22:51:03 +0200

    server

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use proto::agent_server::{Agent, AgentServer};
+use proto::{
+    agent_server::{Agent, AgentServer},
+    RegisterAgentResponse,
+};
+use tonic::{transport::Server, Response};
 mod proto {
     tonic::include_proto!("registration");
 }
 
 #[derive(Debug, Default)]
 struct RegistrationService {}
+
 #[tonic::async_trait]
 impl Agent for RegistrationService {
     async fn register_agent(
         &self,
         request: tonic::Request<proto::Health>,
     ) -> Result<tonic::Response<proto::RegisterAgentResponse>, tonic::Status> {
+        Ok(Response::new(RegisterAgentResponse {
+            id: "1".to_string(),
+        }))
     }
 }
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:5001".parse()?;
+
+    let reg = RegistrationService::default();
+    Server::builder()
+        .add_service(AgentServer::new(reg))  // Corrected line
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
